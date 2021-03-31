f5dfa192532e6fffeb1241b63f7411b02bbdd23c
DaveCommits <79982530+DaveCommits@users.noreply.github.com>
2021-03-31 15:53:51 +0200

    refactored swipeGallery

--- resources/js/swipeGallery.js
+++ resources/js/swipeGallery.js
@@ -1,22 +1,15 @@
-let radioGroup2019 = document.getElementsByName("radio-btn2019");
-
-const display2019 = document.getElementById("display2019");
 const w = window.innerWidth;
-
-const N = display2019.children.length;
 
 function unify(e) { return e.changedTouches ? e.changedTouches[0] : e };
 
 let x0 = null;
-
 function lock(e) { x0 = unify(e).clientX };
 
-let i = 0;
-
-function radio2019Selected() {
-  for (i = 0; i < 11; i++) {
-    if(radioGroup2019[i].checked) {
-      break;
+function radioSelected(radioGroup) {
+  let N = radioGroup.length;
+  for (let i = 0; i < N ; i++) {
+    if(radioGroup[i].checked) {
+      return i;
     }
   }
 }
@@ -24,19 +17,31 @@ function radio2019Selected() {
 function move(e) {
   if(x0 || x0 === 0) {
     let dx = unify(e).clientX - x0, s = Math.sign(dx);
-    radio2019Selected();
-    console.log(dx*s/w);
-    if((i > 0 || s < 0) && (i < N - 1 || s > 0) && ((dx*s/w) > 0.2)){
+    let display = e.currentTarget;
+    let year = display.className.slice(display.className.indexOf("20"));
+    let radioGroup = document.getElementsByName("radio-btn"+year);
+    let N = radioGroup.length;
+    let i = radioSelected(radioGroup);
+    if((i > 0 || s < 0) && (i < N - 1 || s > 0) && ((dx*s/w) > 0.15)){
         i-=s;
-        radioGroup2019[i].checked = true;
+        radioGroup[i].checked = true;
     }
 
     x0 = null
   }
 };
 
+const display2019 = document.getElementsByClassName("display y2019")[0];
+const display2018 = document.getElementsByClassName("display y2018")[0];
+
 display2019.addEventListener('mousedown', lock, false);
 display2019.addEventListener('touchstart', lock, false);
 
 display2019.addEventListener('mouseup', move, false);
 display2019.addEventListener('touchend', move, false);
+
+display2018.addEventListener('mousedown', lock, false);
+display2018.addEventListener('touchstart', lock, false);
+
+display2018.addEventListener('mouseup', move, false);
+display2018.addEventListener('touchend', move, false);
